3c5554b44cbd4c4e59ac616559546a857149e88d
abnud1 <ahmad13932013@hotmail.com>
2017-07-10 20:41:41 +0300

    fixed play-pause button

--- src/content_scripts/videocs.js
+++ src/content_scripts/videocs.js
@@ -15,8 +15,16 @@ function myMain() {
         });
         const recordIcon = '<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#ff0000" d="M19,12C19,15.86 15.86,19 12,19C8.14,19 5,15.86 5,12C5,8.14 8.14,5 12,5C15.86,5 19,8.14 19,12Z" /></svg>'
               ,stopIcon = '<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#000000" d="M18,18H6V6H18V18Z" /></svg>'
-              ,pauseIcon = '<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#000000" d="M14,19H18V5H14M6,19H10V5H6V19Z" /></svg>'
-              ,playIcon = '<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#000000" d="M11,10 L17,10 17,26 11,26 M20,10 L26,10 26,26 20,26" /></svg>'
+              ,playPauseIcon = '<svg width="24px" height="24px" viewBox="0 0 36 36" version="1.1">'+
+      '<defs>'+
+         '<path id="playpausepath" fill="#000000" d="M 11 10 L 17 10 L 17 26 L 11 26 M 20 10 L 26 10 L 26 26 L 20 26">'+
+            '<animate class="animation" begin="indefinite" attributeType="XML" attributeName="d" fill="freeze" from="M11,10 L17,10 17,26 11,26 M20,10 L26,10 26,26 20,26" to="M11,10 L18,13.74 18,22.28 11,26 M18,13.74 L26,18 26,18 18,22.28" dur="0.1s" keySplines=".4 0 1 1"'+
+            'repeatCount="1"></animate>'+
+         '</path>'+
+      '</defs>'+
+      '<use xlink:href="#playpausepath"></use>'+
+      '<use xlink:href="#playpausepath"></use>'+
+   '</svg>'
         let isIconRecord = true;
         let isIconPause = true;
         var recordCallBack = function (event) {
@@ -33,33 +41,35 @@ function myMain() {
                 videoElement.onseeked = () => recorder.resumeRecording();
                 videoElement.onpause = () => recorder.pauseRecording();
                 videoElement.onended = () => {
-                    recorder.stopRecording((blob)=>{
+                    recorder.stopRecording(()=>{
                         recorder.save('video.mp4');
-                        console.log(blob);
                     });
                 }
                 videoElement.onplay = () => recorder.resumeRecording();
                 videoElement.onwaiting = () => recorder.pauseRecording();
                 event.currentTarget.innerHTML = stopIcon;
             } else{
-                recorder.stopRecording((blob)=>{
+                recorder.stopRecording(()=>{
                     recorder.save('video.mp4');
-                    console.log(blob);                    
                 });
                 event.currentTarget.innerHTML=recordIcon;                
             }
             isIconRecord = !isIconRecord;
         };
-
+        let pause = "M11,10 L18,13.74 18,22.28 11,26 M18,13.74 L26,18 26,18 18,22.28",
+            play = "M11,10 L17,10 17,26 11,26 M20,10 L26,10 26,26 20,26";
         var pauseCallBack = function (event) {
+            const animate = event.currentTarget.getElementsByClassName('animation')[0];
             if(isIconPause){
                 recorder.pauseRecording();
-                event.currentTarget.innerHTML = playIcon;
             } else{
                 recorder.resumeRecording();
-                event.currentTarget.innerHTML=pauseIcon;
             }
             isIconPause = !isIconPause;
+            $(animate).attr({
+                "from": isIconPause ? pause : play,
+                "to": isIconPause ? play : pause
+            }).get(0).beginElement();
         };
        
         var btnNames = [
@@ -67,7 +77,7 @@ function myMain() {
                 name: "pauseBtn",
                 order: 1,
                 onclickAction: pauseCallBack,
-                button: pauseIcon
+                button: playPauseIcon
             },
             {
                 name: "recordBtn",
