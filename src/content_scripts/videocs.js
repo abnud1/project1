6a71581106cf017398768e64150589d8b5872e7a
abnud1 <ahmad13932013@hotmail.com>
2017-07-10 16:41:17 +0300

    Changes to be committed: 	modified:   src/content_scripts/videocs.js fixed: when user move in the video with the slider the save dialog doesn't show on clicking stop Button fixed: buttons background weren't visible merged stop and start buttons into one and fliped icons and functionality on click

--- src/content_scripts/videocs.js
+++ src/content_scripts/videocs.js
@@ -1,89 +1,104 @@
 window.addEventListener("load", myMain, false);
 
-function myMain(evt) {
-    var videoElement = document.getElementsByTagName("video")[0];
-
-    var recordCallBack = function () {
-        alert("record button clicked");
-    };
-
-    var stopCallBack = function () {
-        alert("stop button clicked");
-    };
-
-    var pauseCallBack = function () {
-        alert("pause button clicked");
-    };
-
-    var btnNames = [
-        {
-            name: "stopBtn",
-            order: 1,
-            onclickAction: stopCallBack,
-            button: '<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#000000" d="M18,18H6V6H18V18Z" /></svg>'
-        },
-        {
-            name: "pauseBtn",
-            order: 2,
-            onclickAction: pauseCallBack,
-            button: '<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#000000" d="M14,19H18V5H14M6,19H10V5H6V19Z" /></svg>'
-        },
-        {
-            name: "recordBtn",
-            order: 3,
-            onclickAction: recordCallBack,
-            button: '<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#ff0000" d="M19,12C19,15.86 15.86,19 12,19C8.14,19 5,15.86 5,12C5,8.14 8.14,5 12,5C15.86,5 19,8.14 19,12Z" /></svg>'
-        }
-    ];
-    
-    var btnWidth = "30";
-    var btnHeight = "30";
-
-    btnNames.forEach(function (item) {
-        var element = document.createElement("button");
-        element.innerHTML = item.button;
-        element.name = item.name;
-        element.style = "position: absolute; " +
-            "top: " + (videoElement.style.height.split("px")[0] / 2 - btnHeight * item.order) + "px; " +
-            "left: " + (videoElement.style.width.split("px")[0] - btnWidth) + "px;" +
-            "width: " + btnWidth + "px; padding-left:0px; " +
-            "height: " + btnHeight + "px; padding-right:0px; ";
-        element.onclick = item.onclickAction;
-        videoElement.parentNode.insertBefore(element, videoElement.nextSibling);
-    })
-}
-
-
 Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
     get: function () {
         return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
     }
 });
 
+function myMain() {
+    var videoElement = document.getElementsByTagName("video")[0];
+    if(videoElement!==undefined && videoElement!==null){
+        const recorder =  new RecordRTC(videoElement.captureStream(),{
+            disableLogs : true,
+            frameInterval : 24,          
+        });
+        const recordIcon = '<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#ff0000" d="M19,12C19,15.86 15.86,19 12,19C8.14,19 5,15.86 5,12C5,8.14 8.14,5 12,5C15.86,5 19,8.14 19,12Z" /></svg>'
+              ,stopIcon = '<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#000000" d="M18,18H6V6H18V18Z" /></svg>'
+              ,pauseIcon = '<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#000000" d="M14,19H18V5H14M6,19H10V5H6V19Z" /></svg>';
+        let isIconRecord = true;
+        let isIconPause = true;
+        var recordCallBack = function (event) {
+            if(isIconRecord){
+                if(!videoElement.playing){
+                    videoElement.onplaying = () => {
+                        recorder.startRecording();
+                    }
+                }
+                else{
+                    recorder.startRecording();
+                }
+                videoElement.onseeking = () => recorder.pauseRecording();
+                videoElement.onseeked = () => recorder.resumeRecording();
+                videoElement.onpause = () => recorder.pauseRecording();
+                videoElement.onended = () => {
+                    recorder.stopRecording((blob)=>{
+                        recorder.save('video.mp4');
+                        console.log(blob);
+                    });
+                }
+                videoElement.onplay = () => recorder.resumeRecording();
+                videoElement.onwaiting = () => recorder.pauseRecording();
+                event.currentTarget.innerHTML = stopIcon;
+            } else{
+                recorder.stopRecording((blob)=>{
+                    recorder.save('video.mp4');
+                    console.log(blob);                    
+                });
+                event.currentTarget.innerHTML=recordIcon;                
+            }
+            isIconRecord = !isIconRecord;
+        };
+
+        var pauseCallBack = function (event) {
+            if(isIconPause){
+                recorder.pauseRecording();
+            } else{
+                recorder.resumeRecording();
+                event.currentTarget.innerHTML=pauseIcon;
+            }
+            isIconPause = !isIconPause;
+        };
+       
+        var btnNames = [
+            {
+                name: "pauseBtn",
+                order: 1,
+                onclickAction: pauseCallBack,
+                button: pauseIcon
+            },
+            {
+                name: "recordBtn",
+                order: 2,
+                onclickAction: recordCallBack,
+                button: recordIcon
+            }
+        ];
+        
+        var btnWidth = 30;
+        var btnHeight = 30;
+
+        btnNames.forEach(function (item) {
+            var element = document.createElement("button");
+            element.innerHTML = item.button;
+            element.name = item.name;
+            element.style = "position: absolute; " +
+                "top: " + (videoElement.height / 2 - btnHeight * item.order) + "px; " +
+                "left: " + (videoElement.width - btnWidth) + "px;" +
+                "width: " + btnWidth + "px; padding-left:0px; " +
+                "height: " + btnHeight + "px; padding-right:0px; ";
+            element.onclick = item.onclickAction;
+            videoElement.parentNode.insertBefore(element, videoElement.nextSibling);
+        })
+    }
+}
+
+
+
+
 const videos = document.getElementsByTagName("video");
 if (videos.length !== 0) {
     const firstvideo = videos[0];
-    /*firstvideo.onloadeddata = (video) => {
-        const recorder = new RecordRTC(firstvideo.captureStream(),{
-            disableLogs: true,
-            frameInterval : 24,          
-        });
-        if(!video.playing){
-            video.onplaying = () => {
-                recorder.startRecording();
-            }
-        }
-        else{
-            recorder.startRecording();
-        }
-        video.onpause = () => recorder.pauseRecording();
-        video.onended = () => {
-            recorder.stopRecording(()=>{
-                recorder.save('video.mp4');
-            })
-        }
-        video.onplay = () => recorder.resumeRecording();
-        video.onwaiting = () => recorder.pauseRecording();
-    }*/
+   
 }
 
